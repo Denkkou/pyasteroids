ed0f60b01033153555a6122eda1e12bf25a89e8f
Denkkou <JoeSchoDev@gmail.com>
2025-07-06 16:52:32 +0300

    Added constants, created window and game loop

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from pyasteroids!")
+import pygame
+from constants import *
 
+def main():
+    if pygame.get_init() == False:
+        pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
