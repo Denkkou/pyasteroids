396f8f98bda4a65662bfcf0ab76f29b61664ce43
Denkkou <JoeSchoDev@gmail.com>
2025-07-06 22:19:56 +0300

    Created CircleShape base class, with player as child, simple player draw

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     if pygame.get_init() == False:
         pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     # game loop
     while True:
@@ -13,12 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # fill screen black
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
