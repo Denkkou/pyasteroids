db2afdcf549cf66792bdf9d9b241935aac29de40
Denkkou <JoeSchoDev@gmail.com>
2025-07-07 10:48:30 +0300

    Added player rotation and movement

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        # fill screen black
         screen.fill((0, 0, 0))
+        
+        player.update(dt)
         player.draw(screen)
+        
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
