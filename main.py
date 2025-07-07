bf81b5bdadbe36f6d6a1bb5afd65c47f1c817b38
Denkkou <JoeSchoDev@gmail.com>
2025-07-07 11:14:18 +0300

    Asteroids now spawn from screen edges

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     if pygame.get_init() == False:
@@ -9,8 +11,17 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     # game loop
     while True:
@@ -18,11 +29,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill((0, 0, 0))
         
-        player.update(dt)
-        player.draw(screen)
-        
+        for obj in drawable:
+            obj.draw(screen)
+ 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
